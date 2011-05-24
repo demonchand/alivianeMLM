190a6b0077d59e7af15b3fb5b67a8fccabb76e33
chandru <chandru@xtrafin.net>
2011-05-24 13:45:40 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AlivianeMLM::Application.config.secret_token = '32fb2a60cf0a5f42eb26822e395f2a768dd8599a504cb0de640f51e7c356c6fc7c5a6b01540e79125d9035aa3056461eb7f4f9b3ced6fd76cced1bbad6c48f03'
