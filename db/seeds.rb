131cc9d4ed1af73ec5d0c08c3f03410fb08f617f
chandru <chandru@xtrafin.net>
2011-05-24 14:19:51 +0530

    users details added to the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Mayor.create(:name => 'Daley', :city => cities.first)
+
+User.create(:email => "abc@gmail.com", :password => 123456, :password_confirmation => 123456, :role => "Admin")
+User.create(:email => "sudhir@xtrafin.net", :password => 123456, :password_confirmation => 123456, :role => "Admin")
