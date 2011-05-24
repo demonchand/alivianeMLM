e780a414738b73726122b098df2662958190e22f
chandru <chandru@xtrafin.net>
2011-05-24 13:57:34 +0530

    home controller index page is set as root

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ AlivianeMLM::Application.routes.draw do
 
   devise_for :users
 
+  root :to => "home#index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
